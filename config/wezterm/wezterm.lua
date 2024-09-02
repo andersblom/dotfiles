e9032607cc8d843e6cecc6ebd8cbb3559e501dd3
Anders Blom <anders@andersblom.dk>
2024-09-02 11:51:14 -0500

    wezterm window spacing

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -15,4 +15,12 @@ config.line_height = 1.1
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
+-- Window specification
+config.window_padding = {
+	left = 8,
+	right = 8,
+	top = 8,
+	bottom = 8,
+}
+
 return config
