fd0d9fe8b205cb42142dca89aece8b219af1c2ac
Anders Blom <anders@andersblom.dk>
2024-09-02 11:01:39 -0500

    wezterm config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,6 +1,14 @@
 local wezterm = require("wezterm")
-local config = {}
+local config = wezterm.config_builder()
 
-config.color_scheme = "Batman"
+-- Theme specification
+config.color_scheme = "tokyonight_night"
+
+-- Font specification
+config.font = wezterm.font("Hack Nerd Font Mono", {
+	weight = "Regular",
+})
+config.font_size = 16.0
+config.line_height = 1.1
 
 return config
