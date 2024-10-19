a8597b1697aff569a6ff9ca8c465e35ac5ca48fd
Anders Blom <anders@andersblom.dk>
2024-10-19 10:07:19 -0500

    alpha config

--- config/nvim/lua/custom/plugins/alpha.lua
+++ config/nvim/lua/custom/plugins/alpha.lua
@@ -1,7 +1,35 @@
+local function configure()
+	local theme = require("alpha.themes.dashboard")
+	local themeconfig = theme.config
+
+	local dashboard = require("alpha.themes.dashboard")
+
+	-- Set header
+	dashboard.section.header.val = {
+		"███╗   ██╗██╗   ██╗██╗███╗   ███╗",
+		"████╗  ██║██║   ██║██║████╗ ████║",
+		"██╔██╗ ██║██║   ██║██║██╔████╔██║",
+		"██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║",
+		"██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║",
+		"╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝",
+	}
+
+	dashboard.section.buttons.val = {
+		dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+		dashboard.button("<SPC> s-f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
+		dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+	}
+
+	local fortune = require("alpha.fortune")
+	dashboard.section.footer.val = fortune()
+
+	return themeconfig
+end
+
 return {
 	"goolord/alpha-nvim",
 	dependencies = { "echasnovski/mini.icons" },
 	config = function()
-		require("alpha").setup(require("alpha.themes.dashboard").config)
+		require("alpha").setup(configure())
 	end,
 }
